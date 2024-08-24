3e6a79dfd901402c42841591c74071c55cbd4a9f
Hashini Divyanjanee <hashinidivyanjanee1999@gmail.com>
2024-08-24 22:37:08 +0100

    colors

--- lib/Constant/Colors.dart
+++ lib/Constant/Colors.dart
@@ -1,4 +1,13 @@
 import 'dart:ui';
 class CustomColor{
-  static const Color primary = Color(0X2427563);
+  static const Color scaffoldBg = Color(0Xff252734);
+  static const Color bgLight1 = Color(0Xff333646);
+  static const Color bgLight2 = Color(0Xff424657);
+ static const Color textFieldbg = Color(0XffC8C9CE);
+  static const Color hintDark = Color(0Xff666874);
+  static const Color yellowSecondary = Color(0Xffffc25c);
+  static const Color yellowPrimary = Color(0Xffffaf29);
+  static const Color whitePrimary = Color(0XffEAEAEB);
+  static const Color whiteSecondary = Color(0XffC8C9CE);
+
 }
